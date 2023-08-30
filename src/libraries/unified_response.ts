8f56fa1ef2abd1eb663ccf0c943de93968a2e6da
Abdul Rauf <abdulroufsidhu@github.com>
2023-08-30 19:05:49 +0500

    success default message removed

--- src/libraries/unified_response.ts
+++ src/libraries/unified_response.ts
@@ -4,12 +4,12 @@ interface UnifiedResponseProps<T extends any> {
   res: Response;
   code?: number;
   message?: string;
-  data: T;
+  data?: T;
 }
 export const successResponse = <T extends any>({
-  res, code = 200, message = "Success", data
+  res, code = 200, message = "", data
 }: UnifiedResponseProps<T>) => res.status(code).json({ status: code, message, data })
 
 export const errorResponse = <T extends object>({
-  res, code = 500, message = "Error", data
+  res, code = 500, message = "Error", data = <T>{}
 }: UnifiedResponseProps<T>) => res.status(code).json({ status: code, message, data })
